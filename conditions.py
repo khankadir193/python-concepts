6f7907df5e6e1f2464fc91e935057f86b9cb192b
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-07 17:38:06 +0530

    python basic concepts

---
+++ conditions.py
@@ -0,0 +1,15 @@
+#many value alongwith multiple variables
+x,y,z = 10,30,40;
+print(x);
+print(y);
+print(z);
+
+#one value along with multiple variable
+x=y=z = 40;
+print('x',x,'y',y,'z',z);
+
+#if you have collection of value like list,tuple etc. we can extract those value into
+# a variable.
+
+arr = [23,54,53,68];
+
