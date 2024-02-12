10a6eba653fc220e80c65e43d4fbad17a67fd57c
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-12 13:17:33 +0530

    inbuilt functions

---
+++ function.py
@@ -0,0 +1,8 @@
+#function:- in python function is a block of statement.we reuse the code whenever
+#we need and also we can call many times.
+def square(x):
+    return x*x;
+
+result = square(50);
+print('result.../',result);
+
---
+++ inBuilltFunction.py
@@ -0,0 +1,63 @@
+#all function accepts iterable objects such as list,tuple,dict etc.
+#check wether all items it's true or not.
+k = [1,2,4,5];
+print(all(k));
+
+k = [False,0];
+print(all(k));
+
+k =  [True,0,1];
+print(all(k));
+
+k = [];
+print(all(k));
+
+#bin:- python bin fun it is return the binary representation of a specified integer.
+#A result always start prefix ob.
+x = 100;
+print('x..',bin(x));
+
+#bool:- checking wether it's true or not.
+x = [];
+print('[]',bool(x));
+
+x = [4,1];
+print('[4,1]',bool(x));
+
+x = [0];
+print('[0]',bool(x));
+
+x = '';
+print('emptyString..?',bool(x));
+
+x = 'abdul kadir khan';
+print('string..',bool(x));
+
+x = True;
+print('True...',bool(x));
+
+#bytes:- bytes fun used for returning bytes object.
+#it is immutable version of of the bytearray() func.
+
+str = "abdul kadir khan";
+array = bytes(str,'utf-8');
+print('array...',array);
+
+#exec:this fun is used for dynamic execution of program. 
+x = 8;
+exec("print(x==8)");
+exec("print(x==7)");
+
+#sum:-sum fun is used to get the total sum of the iterable object.
+arr = [1,4,7];
+print(sum(arr));
+
+#any func:- return true if any item is true in the iterable otherwise false;
+x = [1,False,4];
+print(any(x));
+
+x = [4,6,3,5];
+print(any(x));
+
+x = [0,False,2];
+print(any(x));
