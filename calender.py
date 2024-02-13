f87ee7b916b1a70b77a9993f794b9e7ee821cfdd
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-13 09:57:09 +0530

    calender functions

---
+++ calender.py
@@ -0,0 +1,9 @@
+import calendar;
+
+#we can get easily particular month and year dates.
+cal = calendar.month(2024,10);
+print('calender..',cal);
+
+#prcal():- along with this method we can print calender of entire year;
+year = calendar.prcal(2024);
+print('whole year calender..',year);
