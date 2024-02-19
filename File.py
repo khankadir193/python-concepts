44166f32a9b819caa0dc004b99b1a10bf742f3ac
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-19 22:03:16 +0530

    opening file with open command

--- File.py
+++ File.py
@@ -11,5 +11,16 @@ file2.write('abdul jabir khan');
 file2.close();  #here i am closing the file..
 print('writing text into the file...??',file2);
 
+#here another approach for opening the file with keyword..
+with open('read.txt','r') as readFile:
+    read_content = readFile.read();
+    print('read_content........))))))))',read_content);
+
+#here current directory........
+#python directory and file management..
+import os;
+
+print('current working directory...',os.getcwd());
+
 
 
